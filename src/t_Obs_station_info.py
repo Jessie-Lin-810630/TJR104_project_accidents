8fd053ffbf9f40632c795e3808e913a072654e14
Jessie_lin <lucky460721@gmail.com>
2026-01-27 18:27:24 +0800

    Change the methods used in step 3 to make the process more robust and stable. Discard the Path(__file__) because it may fail in jupyter notebook.

--- src/t_Obs_station_info.py
+++ src/t_Obs_station_info.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-curr_dir = Path(__file__).resolve().parent
+curr_dir = Path().resolve()
 
 # Step 1: 定義好讀資料時的資料型態。
 col_map = {"站號": {"name": "Station_ID", "Type": object},
@@ -12,7 +12,7 @@ col_map = {"站號": {"name": "Station_ID", "Type": object},
            "資料起始日期": {"name": "Date_of_Opening", "Type": object}
            }
 dtypes = {k: v["Type"] for k, v in col_map.items()}
-df = pd.read_csv(curr_dir.parent /
+df = pd.read_csv(curr_dir /
                  "supplementary_weather_csv_from_CODiS/station_info_table.csv",
                  encoding="utf-8-sig",
                  dtype=dtypes)
@@ -21,13 +21,13 @@ df = pd.read_csv(curr_dir.parent /
 col_name = {k: v["name"] for k, v in col_map.items()}
 df_weather_obs_stations = df.rename(columns=col_name)
 
-# Step 3: 確保資料起始日期為datetime格式
+# Step 3: 確保資料起始日期為datetime認可的格式
 pd.to_datetime(
-    df_weather_obs_stations["Date_of_Opening"], errors="coerce").astype(str)
+    df_weather_obs_stations["Date_of_Opening"], format="%Y%m%d", errors="coerce").dt.strftime("%Y-%m%-d")
 
 if __name__ == "__main__":
     # Step 3: 存檔。
-    df_weather_obs_stations.to_csv(curr_dir.parent /
+    df_weather_obs_stations.to_csv(curr_dir /
                                    "supplementary_weather_csv_from_CODiS/station_info_table_Eng.csv",
-                                   encoding="utf-8",
+                                   encoding="utf-8-sig",
                                    index=False)  # 不用匯入index，因為站號已經是有識別用了
