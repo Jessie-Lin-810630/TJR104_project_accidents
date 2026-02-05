c58d54818d04b41a049c7ff73fa3599c806fa912
Jessie_lin <lucky460721@gmail.com>
2026-02-06 01:13:16 +0800

    changed the format of created_on, updated_on, and pk

--- util/create_hist_weather_data_table.py
+++ util/create_hist_weather_data_table.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
 from dotenv import load_dotenv
-
+from urllib.parse import quote_plus
+from pathlib import Path
 
 # 建立Weather_data資料表
 load_dotenv("./src/.env")
@@ -11,6 +12,15 @@ server = "127.0.0.1:3306"
 DB = "TESTDB"
 engine = create_engine(f"mysql+pymysql://{username}:{password}@{server}/{DB}",)
 
+# or, Connect to GCP VM MySQL server
+# load_dotenv("./src/.env")
+# username = quote_plus(os.getenv("mysql_username"))
+# password = quote_plus(os.getenv("mysql_password"))
+# server = "127.0.0.1:3307"
+# DB = "test_weather"
+# engine = create_engine(
+#     f"mysql+pymysql://{username}:{password}@{server}/{DB}",)
+
 
 def create_hist_weahter_data_table(engine, table_name: str) -> None:
     """Create the table 'historical_weather_data' into the assigned database
@@ -18,6 +28,7 @@ def create_hist_weahter_data_table(engine, table_name: str) -> None:
     Raise Error when the table is already exist. '"""
     with engine.connect() as conn:
         ddl_query = text(f"""CREATE TABLE {table_name}(
+                                `Hash_value` CHAR(64) NOT NULL,
                                 `Station_record_id` INT AUTO_INCREMENT COMMENT '測站紀錄流水編號',
                                 `Station_id` VARCHAR(10) COMMENT '觀測站別',
                                 `Observation_datetime` DATETIME COMMENT '氣象觀測日期(yyyy/mm/dd HH:MM:00)',
@@ -46,11 +57,11 @@ def create_hist_weahter_data_table(engine, table_name: str) -> None:
                                 `Soil_temp_at_30_cm` DECIMAL(6,1) COMMENT '地溫30cm',
                                 `Soil_temp_at_50_cm` DECIMAL(6,1) COMMENT '地溫50cm',
                                 `Soil_temp_at_100_cm` DECIMAL(6,1) COMMENT '地溫100cm',
-                                `Created_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '建立日期',
-                                `Created_by` VARCHAR(50) COMMENT '建立者',
-                                `Updated_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '最近修改日期',
+                                `Created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '建立日期',
+                                `Created_by` VARCHAR(50) NOT NULL COMMENT '建立者',
+                                `Updated_on` TIMESTAMP COMMENT '最近修改日期',
                                 `Updated_by` VARCHAR(50) COMMENT '修改者',
-                                PRIMARY KEY (`Station_ID`, `Observation_datetime`),
+                                PRIMARY KEY (`Hash_value`),
                                 CONSTRAINT FK_HWD_StnID FOREIGN KEY (`Station_record_id`)
                                             REFERENCES Obs_stations(`Station_record_id`))
                                 CHARSET=utf8mb4 COMMENT '各觀測站天氣觀測結果';""")
